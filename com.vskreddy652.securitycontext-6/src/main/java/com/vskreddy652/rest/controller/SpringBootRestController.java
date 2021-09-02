40ac5b28f9d446a062d2710e58be7ba9fc196694
vskreddy652 <vskreddy652@yahoo.com>
2021-09-02 11:46:33 +0530

    Add files via upload

---
+++ com.vskreddy652.securitycontext-6/src/main/java/com/vskreddy652/rest/controller/SpringBootRestController.java
@@ -0,0 +1,36 @@
+package com.vskreddy652.rest.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// http://localhost:9229/springbootwithsecurity/userlogin1
+// http://localhost:9229/springbootwithsecurity/adminlogin
+
+@RestController
+public class SpringBootRestController {
+	@RequestMapping("/userlogin1")
+	public String userValidation1() {
+		UserDetails user_details = (UserDetails)SecurityContextHolder
+									.getContext()
+									.getAuthentication().getPrincipal();
+
+		return "User: Successfully logged in 1111!"+user_details.getUsername();
+	}
+
+	@RequestMapping("/userlogin2")
+	public String userValidation2() {
+		return "User: Successfully logged in 2222!";
+	}
+
+	@RequestMapping("/adminlogin")
+	public String adminValidation() {
+		return "Admin: Successfully logged in!";
+	}
+
+	@RequestMapping("/slrlogin")
+	public String slrValidation() {
+		return "Seller: Successfully logged in!";
+	}
+}
